fa878bb977ed41726fc7abe17a99121d6c800d38
20revsined <44682153+20revsined@users.noreply.github.com>
2018-11-20 17:33:40 -0500

    Update and rename README.md to TextAdventure.py

---
+++ TextAdventure.py
@@ -0,0 +1,36 @@
+class Profile():
+	def __init__(self, name, residence):
+		self.name = name
+		self.residence = residence
+    
+class Inventory():
+	def __init__(self, weapons, money, arrows):
+		self.weapons = weapons
+		self.money = money
+		self.arrows = arrows
+		weapons = "sword", "bow and arrows"
+		money = 0
+    
+  def __init__(self, w, m, a):
+		weapons = w
+		money = m
+		arrows = a
+    
+   def AddMoney(x):
+		return money += x
+    
+   def Ammunition(x):
+		return x -= 1
+
+
+name = str(input("Welcome to the text adventure! Please start by entering your name."))
+residence = str(input("Okay," + name + ", where do you live in Fjordland?"))
+travel = str(input("Okay," + name + ", you leave " + residence + ", and walk to Castle City. " + 
+	"Someone asks you for directions. What will you do?\n Help them out\n Kill them\n"))
+
+switch(travel):
+	case("Help them out"):
+		print("Nice job, you gain 500 coins for your trouble.")
+		balance = 500
+	case("Kill them"):
+		print("Oh no! The king's soldiers are after you! You only have a sword and a bow and arrows. What would you like to do? \nFight them\n Surrender")
